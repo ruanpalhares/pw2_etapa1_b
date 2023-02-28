6c145e52956c184e6c972d7efa83346e2733889c
victoricoma <victorsena3010@gmail.com>
2023-02-27 22:43:48 -0300

    Eventos Aula2702

--- Aula2702/index.js
+++ Aula2702/index.js
@@ -2,3 +2,22 @@ const chalk = require('chalk')
 const inquirer = require('inquirer')
 const calculadora = require('calculadora')
 
+inquirer.prompt([
+    {
+        name:'t1',
+        message:'Qual a nota do Trabalho 1: '
+    },
+    {
+        name:'t2',
+        message:'Qual a nota do Trabalho 2: '
+    },
+    {
+        name:'p3',
+        message:'Qual a nota da Prova 3: '
+    },
+    {
+        name:'p4',
+        message:'Qual a nota da Prova 4: '
+    }
+]).then()
+.catch()
