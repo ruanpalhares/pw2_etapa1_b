27a1f6bcc43a1b70771bc756783e918bb7b25eed
victoricoma <victorsena3010@gmail.com>
2023-03-22 20:29:14 -0300

    Aula 2203 contexto de multiplos cenários e FileSystem

--- Aula1403/index.js
+++ Aula1403/index.js
@@ -6,6 +6,7 @@ const inquirer = require('inquirer')
 //#region Módulos Internos
 const fs = require('fs')
 const { type } = require('os')
+const { clearScreenDown } = require('readline')
 //#endregion
 
 operation()
@@ -69,6 +70,7 @@ function buildAccount(){
         if(fs.existsSync(`accounts/${accountName}.json`)){
             console.log(chalk.bgRed.black('Esta conta já existe!'))
             buildAccount(accountName)
+            return
         }
 
         fs.writeFileSync(
@@ -84,3 +86,49 @@ function buildAccount(){
 }
 //#endregion
 
+//#region Depositar na Conta
+function deposit(){
+    inquirer.prompt([
+        {
+            name:'accountName',
+            message:'Para qual conta irá o depósito?'
+        }
+    ]).then((answer) => {
+        const accountName =answer['accountName']
+        if(!checkAccount(accountName)){
+            return deposit()
+        }
+
+        inquirer.prompt([
+            {
+                name:'amount',
+                message: 'Quanto você deseja depositar?'
+            }
+        ]).then((answer) =>{
+            const amount = answer['amount']
+            addAmount(accountName, amount)
+            console.log(chalk.bgYellow.green('Sucesso! Seu montante foi depositado.'))
+            setTimeout(() => {
+                operation()
+            }, 1000);
+        })
+    })
+}
+function checkAccount(accountName){
+    if(!fs.existsSync(`accounts/${accountName}.json`)){
+        console.log(chalk.bgRed.black('Esta conta não existe.'))
+        return false
+    }
+    return true
+}
+function getAccount(accountName){
+    const accountJSON = fs.readFileSync(`accounts/${accountName}.json`,{
+        encoding:'utf8',
+        flag:'r'
+    })
+    return JSON.parse(accountJSON)
+}
+function addAmount(accountName, amount){
+
+}
+//#endregion
